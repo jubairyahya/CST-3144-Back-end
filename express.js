59adee63492bfd504490cba3ab003d7de1391530
jubairyahya <yemjubair@yahoo.com>
2025-11-24 15:18:17 +0000

    added logger middleware

--- express.js
+++ express.js
@@ -18,6 +18,12 @@ app.use(cors());
 app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
+//logger middleware
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
+
 //mongodb connection
 const user = encodeURIComponent(process.env.DB_USER);
 const pass = encodeURIComponent(process.env.DB_PASS);
@@ -55,7 +61,7 @@ const upload = multer({ storage });
 
 
 app.get("/", (req, res) => {
-  res.send("Backend is running 🚀");
+  res.send("Backend is running ");
 }); 
 //admin 
 const ADMIN_USERNAME = 'zuzu';
